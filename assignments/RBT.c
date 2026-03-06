f400e585f3a3b9ec7e333410d09d2978eefdf709
mark3r13 <belemarko@yahoo.ro>
2026-03-06 20:46:53 +0200

    Insert in progress

--- assignments/RBT.c
+++ assignments/RBT.c
@@ -26,25 +26,25 @@ bool key_sm(signed short n_key, signed short s_key) { return s_key < n_key; }
 
 bool is_tree_null(tree bst) { return !bst.rt; }
 
-node* search_node(node *n, signed short key, node *p, bool parent) {
+node* search_node(node *n, signed short key) {
     if(!n) return NULL;
 
-    if(search_found(n, key)) return parent ? p : n;
+    if(search_found(n, key)) return n;
     else {
         signed short cur = n->key;
         if(key_sm(cur, key)) {
-            if(n->lft) return search_node(n->lft, key, n, parent);
+            if(n->lft) return search_node(n->lft, key);
             else       return NULL;
         } else { // Implied else is key >= cur. The case key == cur can never occur because of the search_found check. Thus, we are left with key > cur.
-            if(n->rgt) return search_node(n->rgt, key, n, parent);
+            if(n->rgt) return search_node(n->rgt, key);
             else       return NULL;
         }
     }
 }
 
-node* search(tree bst, signed short key, bool parent) {
+node* search(tree bst, signed short key) {
     if(is_tree_null(bst)) return NULL;
-    return search_node(bst.rt, key, NULL, parent);
+    return search_node(bst.rt, key);
 }
 
 
@@ -76,12 +76,13 @@ tree* create_tree(node *rt) {
     return t;
 }
 
-node* create_node(signed short key) {
+node* create_node(signed short key, node *pnt) {
     node *n = malloc(sizeof(node));
     if(!n) { perror("malloc"); return NULL; }
     n->key = key;
     n->lft = NULL;
     n->rgt = NULL;
+    n->pnt = pnt;
     n->col = true; // Red by default makes it insert friendly
 }
 
@@ -91,6 +92,7 @@ void insert_node(node* p, bool dir, node *c, node *n) {
     if(!c) {
         if(!dir) p->lft = n;
         else     p->rgt = n;
+        n->pnt = p;
         return;
     }
     signed short c_key = c->key;
@@ -100,47 +102,61 @@ void insert_node(node* p, bool dir, node *c, node *n) {
 } // p = Parent node, dir : false = left, true  = right, c = Current node, n = Newly added node
 
 void rotate_left(tree *t, node *n) {
-    node *c = n->rgt;
-    if(!c) return;
-    n->rgt = c->lft;
-    if(c->lft != t->rt) c->lft = n;
-    node *n_p = search(*t, n->key, true);
-    node *c_p = search(*t, c->key, true);
-    c_p = n_p;
-    if(n_p == t->rt)       t->rt = c;
-    else if(n == n_p->lft) n_p->lft = c;
-    else                   n_p->rgt = c;
-    c->rgt = n;
-    n_p = c;
 }
 
-void insert_fixup(tree *t, signed short key) {
-    node *p = search(*t, key, true); // Parent
-    node *g = search(*t, p->key, true); // Grandparent
-    node *u = p == g->lft ? g->rgt : g->lft;  // Uncle
-    while(p->col) { // While parent's color is red
-        if(u->col) { // Uncle color is red
+void insert_fixup(tree *t, node *n) {
+    if(n == t->rt) { n->col = false; return; } // Root case, should be BLACK
+    node *p = n->pnt; // Parent
+    if(!p->col) return; // If parent's color is BLACK
+    node *g = p->pnt; // Grandparent
+    node *u = NULL;
+    if(p == g->lft) { // Uncle
+        u = g->rgt;
+        if(u != t->rt && u->col) { // If uncle exists and is RED
+            p->col = false; // BLACK
+            u->col = false; // BLACK
+            g->col = true;  // RED
+            insert_fixup(t, g);
+        } else { 
+            if(n == p->rgt) { // Triangle formation -- PROVIDE PHOTO EXAMPLE
+                //left_rotate p
+                node *tmp = n->pnt;
+                n = p;
+                p = tmp;
+            }
+            // Line formation
+            p->col = false; // BLACK
+            g->col = true;  // RED
+            //right_rotate g
+        }
+    } else {
+        u = g->lft;
+        if(u != t->rt && u->col) {
+            p->col = false; // BLACK
+            u->col = false; // BLACK
+            g->col = true;  // RED
+            insert_fixup(t, g);
+        } else { // Mirrored
+            if(n == p->lft) {
+                //right_rotate p
+                node *tmp = malloc(sizeof(node));
+                tmp = n->pnt;
+                n = p;
+                p = tmp;
+                free(tmp);
+            }
             p->col = false;
-            u->col = false;
             g->col = true;
-        } else {
-            if(p->rgt->key == key) {
-                // rotate_left(); // TBC
-            }
-            if(p->lft->key == key) {
-                p->col = false;
-                g->col = true;
-                // rotate_right(); // TBC
-            }
+            //left_rotate g
         }
     }
-    insert_fixup(t, p->key);
-}
+    t->rt->col = false;
+} // TO BE FIXED
 
 void insert(tree *t, signed short key) {
     // Binary search tree insertion + Red coloring
-    if(search(*t, key, false)) { node_exists(); return; }
-    node *n = create_node(key);
+    if(search(*t, key)) { node_exists(); return; }
+    node *n = create_node(key, NULL);
     if(!t->rt) t->rt = n;
     else       insert_node(NULL, true, t->rt, n);
     insert_fixup(t, key);
@@ -161,13 +177,13 @@ void printn_null() { printf("ERROR ::: Node is null!\n"); }
 // Trashed this version because: v2 avoids unnecessary 'else' + v2 keeps control flow flatter + v2 scales better in larger functions
 
 void print_search(tree t, signed short key) {
-    node *n = search(t, key, false);
+    node *n = search(t, key);
     if(n) { printn(n); return; }
     printn_null();
 }
 
 void print_parent(tree t, signed short key) {
-    node *n = search(t, key, true);
+    node *n = search(t, key);
     if(n) { printn(n); return; }
     printn_null();
 }
@@ -191,14 +207,21 @@ void endl() { printf("\n"); }
 
 int main() {
     // RBT_sample.jpg
-    node n8 = {150, NULL, NULL, true};
-    node n7 = {110, NULL, NULL, true};
-    node n6 = {145, &n7, &n8, false};
-    node n5 = {76, NULL, NULL, false};
-    node n4 = {32, NULL, NULL, true};
-    node n3 = {21, NULL, &n4, false};
-    node n2 = {60, &n3, &n5, true};
-    node n1 = {100, &n2, &n6, false};
+    node n8 = {150, NULL, NULL, NULL, true};
+    node n7 = {110, NULL, NULL, NULL, true};
+    node n6 = {145, &n7, &n8, NULL, false};
+    node n5 = {76, NULL, NULL, NULL, false};
+    node n4 = {32, NULL, NULL, NULL, true};
+    node n3 = {21, NULL, &n4, NULL, false};
+    node n2 = {60, &n3, &n5, NULL, true};
+    node n1 = {100, &n2, &n6, NULL, false};
+    n2.pnt = &n1;
+    n3.pnt = &n2;
+    n4.pnt = &n3;
+    n5.pnt = &n2;
+    n6.pnt = &n1;
+    n7.pnt = &n6;
+    n8.pnt = &n6;
     tree rbt = {&n1};
 
     header("Search\n");
